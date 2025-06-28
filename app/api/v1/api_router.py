b077d318ca9bfc9b4f6834d249146d6e8ba45fa0
Max-APL <maxpasten15908@gmail.com>
2025-06-28 15:34:57 -0400

    Update webhook prod

--- app/api/v1/api_router.py
+++ app/api/v1/api_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import peques
+from app.api.v1.endpoints import peques, storage
 
 api_router = APIRouter()
 api_router.include_router(peques.router, tags=["Peques"])
